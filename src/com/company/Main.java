8e5525fff8e4b550b7c9bd7f864fe43fe267cb5c
khangnick14 <khangnick14@gmail.com>
2022-01-05 14:54:19 +0700

    update 5/1 Khang

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,11 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    class Node
-    {
+    private static int maxSum = 0;
+    private static Node maxLeaf = null;
+    private static ArrayList<Integer> pathFromMaxToRoot = new ArrayList<>();
+
+    static class Node {
         int data;
         Node left = null, right = null;
 
@@ -29,7 +32,7 @@ public class Main {
                 String line = scan.nextLine();
                 String[] temp = line.split(" ");
                 for (int j = 0; j < col; j++) {
-                    if(temp[j].equals(".")) {
+                    if (temp[j].equals(".")) {
                         maze[i][j] = 0;
                     } else if (temp[j].equals("x")) {
                         maze[i][j] = -1;
@@ -45,18 +48,92 @@ public class Main {
         }
     }
 
+    private static Node constructTree(int[][] maze, Node root, int row, int col) {
+        if ((row < maze.length) && (col < maze[0].length)) {
+            Node temp = new Node(maze[row][col]);
+            root = temp;
+            root.left = constructTree(maze, root.left, row, col + 1);
+            root.right = constructTree(maze, root.right, row + 1, col);
+        }
+        return root;
+    }
+
+    private static void displayMaze(int[][] maze) {
+        int row = maze.length;
+        int col = maze[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(maze[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void findMaximumPath(Node root, int currSum) {
+        //base Case
+        if (root == null) {
+            return;
+        }
+        currSum += root.data;
+        //check the stop condition
+        if (root.left == null && root.right == null) {
+            if (maxSum < currSum) {
+                maxSum = currSum;
+                maxLeaf = root;
+            }
+            return;
+        }
+        findMaximumPath(root.left, currSum);
+        findMaximumPath(root.right, currSum);
+        return;
+    }
+
+    private static void printPreOder(Node node) {
+        if (node == null) {
+            return;
+        }
+        System.out.print(node.data + " ");
+        if (node.left != null && node.left.data == -1) {
+            printPreOder(node.right);
+        } else if (node.right != null && node.right.data == -1) {
+            printPreOder(node.left);
+        } else {
+            printPreOder(node.left);
+            printPreOder(node.right);
+        }
+
+    }
+
+    private static Boolean getPath(Node root, Node leaf) {
+        if (root == null) return false;
+        if ((root == leaf) || getPath(root.left, leaf) || getPath(root.right, leaf)) {
+            pathFromMaxToRoot.add(root.data);
+            return true;
+        }
+        return false;
+    }
 
     public static void main(String[] args) {
         int[][] maze = readFile(newFile);
-        int row = maze.length;
-        int col = maze[0].length;
         readFile(newFile);
-        //display maze
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                System.out.print(maze[i][j]+" ");
-            }
-            System.out.println();
+        displayMaze(maze);
+        System.out.println();
+        //construct Tree
+        Node root = new Node(maze[0][0]);
+        root = constructTree(maze, root, 0, 0);
+
+        //Display constructed tree
+        printPreOder(root);
+        System.out.println();
+
+        //find maximum path and sum
+        findMaximumPath(root, 0);
+        System.out.println("Maximum gold: " + maxSum);
+        getPath(root, maxLeaf);
+        System.out.println("Total steps: " + (pathFromMaxToRoot.size() - 1));
+        System.out.print("Path: ");
+        for(int i : pathFromMaxToRoot) {
+            System.out.print(i + " ");
         }
     }
 }
