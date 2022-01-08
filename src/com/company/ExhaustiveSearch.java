35831223a6cd277c9e1bf06fd801dbaf1988a8ea
khangnick14 <khangnick14@gmail.com>
2022-01-08 16:50:14 +0700

    Update Exhaustive Search Approach

---
+++ src/com/company/ExhaustiveSearch.java
@@ -0,0 +1,97 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.sql.Array;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ExhaustiveSearch {
+    static ArrayList<Path> allPath = new ArrayList<>();
+    private static File newFile = new File("..\\GroupProject_Algo\\src\\com\\company\\mapName.txt");
+
+    private static int[][] readFile(File file) {
+        try {
+            Scanner scan = new Scanner(file);
+            String[] arrRow = scan.nextLine().split(" ");
+            int row = Integer.parseInt(arrRow[0]);
+            int col = Integer.parseInt(arrRow[1]);
+            int[][] maze = new int[row][col];
+            for (int i = 0; i < row; i++) {
+                String line = scan.nextLine();
+                String[] temp = line.split(" ");
+                for (int j = 0; j < col; j++) {
+                    if (temp[j].equals(".")) {
+                        maze[i][j] = 0;
+                    } else if (temp[j].equals("x") || temp[j].equals("X")) {
+                        maze[i][j] = -1;
+                    } else {
+                        maze[i][j] = Integer.parseInt(temp[j]);
+                    }
+                }
+            }
+            return maze;
+        } catch (NullPointerException | FileNotFoundException e) {
+            System.out.println("No file found!");
+            return null;
+        }
+    }
+
+    public static Path helper(int[][] maze) {
+        int row = maze.length;
+        int column = maze[0].length;
+        int count = 0;
+        Path solutionPath = new Path(maze[0][0], new ArrayList<String>(), maze);
+        //Maximum length of path
+        final int MAX_LENGTH = row + column - 2;
+        for (int i = 1; i <= MAX_LENGTH; i++) {
+            for (int j = 0; j <= Math.pow(2.0, i) - 1; j++) {
+                ArrayList<String> collectGoldPath = new ArrayList<>();
+                Path path = new Path(maze[0][0], collectGoldPath, maze);
+                int a = 0, b = 0;
+                aa:
+                for (int k = 0; k < i; k++) {
+                    int temp;
+                    temp = (j >> k) & 1;
+                    //go Right
+                    if (temp == 1) {
+                        if (path.isRightStepValid(a, b)) {
+                            path.getCollectGoldPath().add("R");
+                            b += 1;
+                            path.updateCurrentGold(a, b);
+                        }
+                    }
+                    //go Down
+                    else {
+                        if (path.isDownStepValid(a, b)) {
+                            path.getCollectGoldPath().add("D");
+                            a += 1;
+                            path.updateCurrentGold(a, b);
+                        }
+                    }
+                }
+                if (path.getTotalGold() > solutionPath.getTotalGold()) {
+                    solutionPath = path;
+                    System.out.println(count + ". UPDATED new Solution!!!");
+                    count++;
+                }
+            }
+        }
+        return solutionPath;
+    }
+
+
+    public static void main(String[] args) {
+        int[][] maze = readFile(newFile);
+        Path solutionPath = helper(maze);
+        ArrayList<String> path = solutionPath.getCollectGoldPath();
+        System.out.println("Total gold: " + solutionPath.getTotalGold());
+        System.out.print("Path: ");
+        for (String s : path) {
+            System.out.print(s + " ");
+        }
+        System.out.println();
+        System.out.println("Total steps: " + (path.size()));
+    }
+}
